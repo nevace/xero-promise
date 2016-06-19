35f23c0d65e5374349cfe825aa4cafb726830471
nevace <liam.neville89@gmail.com>
2016-06-19 17:47:53 +0100

    added generator function to GET all data unless page is specified

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-'use strict'
+'use strict';
 
 const crypto = require('crypto');
 const oauth = require('oauth');
 const EasyXml = require('easyxml');
 const inflect = require('inflect');
-
-const XERO_API_URL = 'https://api.xero.com/api.xro/2.0/';
+const querystring = require('querystring');
+const co = require('co');
+const xeroApiUrl = 'https://api.xero.com/api.xro/2.0/';
 
 
 class Xero {
@@ -19,23 +20,65 @@ class Xero {
     };
   }
 
-  call(method, path, body) {
+  call(method, path, body, params) {
+    const fullPath = xeroApiUrl + path;
     let postBody;
     let contentType;
+    params = params || '';
 
     if (method && method !== 'GET' && body) {
-      if (Buffer.isBuffer(body)) {
-        postBody = body;
+      const root = path.match(/([^\/\?]+)/)[1];
+      const promises = [];
+      //get average size of individual xml doc and work out batch number
+
+      //batch
+      postBody = new EasyXml({ rootElement: inflect.singularize(root), rootArray: root, manifest: true }).render(body);
+      contentType = 'application/xml';
+
+      params = params ? `?${querystring.stringify(params)}` : '';
+
+      if (batchedData) {
+        for (let data of batchedData) {
+          promises.push(this._request(method, fullPath, params, data, contentType));
+        }
+        return Promise.all(promises);
       } else {
-        const root = path.match(/([^\/\?]+)/)[1];
-        postBody = new EasyXml({ rootElement: inflect.singularize(root), rootArray: root, manifest: true }).render(body);
-        contentType = 'application/xml';
+        return this._request(method, fullPath, params, data, contentType);
+      }
+
+    } else {
+      //GET request with no params or no page param
+      if (!params || !params.page) {
+
+        return co(function*() {
+          let pageNo = 1;
+          let result;
+          let fullResult = [];
+          params = params ? `?${querystring.stringify(params)}&page=` : '?page=';
+
+          result = yield this._request(method, fullPath, params + pageNo, postBody, contentType);
+          fullResult.push(...result[path]);
+
+          while (result[path].length) {
+            pageNo++;
+            fullResult.push(...result[path]);
+            result = yield this._request(method, fullPath, params + pageNo, postBody, contentType);
+          }
+          return fullResult;
+        }.bind(this));
+
+      } else {
+        //GET request with page param
+        params = `?${querystring.stringify(params)}`;
+        return this._request(method, fullPath, params, postBody, contentType);
       }
     }
+  }
 
+  _request(method, fullPath, params, postBody, contentType) {
     return new Promise((resolve, reject) => {
-
-      this.oa._performSecureRequest(this.key, this.secret, method, XERO_API_URL + path + '?summarizeErrors=false', null, postBody, contentType, (err, data, res) => {
+      console.log(`${method} ${fullPath + params}`);
+      this.oa._performSecureRequest(this.key, this.secret, method, fullPath + params, null, postBody, contentType, (err, data, res) => {
         if (err) {
           try {
             err.data = JSON.parse(err.data);
@@ -44,7 +87,6 @@ class Xero {
             return reject(err);
           }
         }
-
         try {
           const json = JSON.parse(data);
           return resolve(json);
